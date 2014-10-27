2b615d89ac47408369b835cf74c5a05716778dd7
Bill Xiong <billxiong99@gmail.com>
2014-10-27 13:04:27 -0700

    Added Forgot Password link

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -76,6 +76,30 @@ angular.module('clientControllers', ['ngAnimate'])
       $scope.displayUI = true;
     };
 
+    $scope.recoverInformation = function() {
+      $scope.emailRecoverError = false;
+      $scope.emailRecoverMessage = '';
+      $scope.recoverHasSubmitted = true;
+
+      if($scope.recoverInfoForm.$valid) {
+
+        $http.post(StateService.getServerAddress() + 'users/password/reset/', {'email' : $scope.recoverEmail})
+          .success(function (data, status) {
+            console.log("Sent recovery email");
+            angular.element('#recoverInfoModal').modal('hide');
+            $scope.recoverHasSubmitted = false;
+          })
+          .error(function (data, status, headers, config) {
+            // If there's been an error, time to display it back to the user on the form. (These are where server side errors are set)
+            var h = headers();
+            if(h['error-type'] === 'email') {
+              $scope.emailRecoverError = true;
+              $scope.emailRecoverMessage = h['error-message'];
+            }
+        });
+      }
+    }
+
     // Submit the registration form and perform validation on it.
     $scope.submitRegistration = function() {
       $scope.usernamePostError = false;
